b56558270a2cb517979a15712d525be389aac887
Ilya Kubariev <ilya.kubariev@redduck.io>
2024-06-15 15:48:07 +0300

    fix(web): eslint 2

--- apps/frontend/web/src/pages/_app.tsx
+++ apps/frontend/web/src/pages/_app.tsx
@@ -1,10 +1,9 @@
-import Providers from '@/providers/Providers';
-
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { Layout } from '@/components/layout';
+import Providers from '@/providers/Providers';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
