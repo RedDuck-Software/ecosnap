391266820498778bd989ef3d2c42d9f217b0d4f2
MrJeleika <den932005@gmail.com>
2024-06-15 13:58:41 +0300

    feat(web): layout + fonts

--- apps/frontend/web/src/pages/index.tsx
+++ apps/frontend/web/src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <WalletMultiButton />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
