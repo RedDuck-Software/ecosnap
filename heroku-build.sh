cd26f2ecec415303586978809eaf94750381e914
kostyamospan <kostya04042001@gmail.com>
2024-06-16 19:18:43 +0300

    fix: heroku postbuild

--- heroku-build.sh
+++ heroku-build.sh
@@ -1,6 +1,6 @@
 if [ "$BUILD_APP" = "api" ]; 
         then yarn build:api; 
-    else if [ "$BUILD_APP" = "submitter" ]; 
+    elif [ "$BUILD_APP" = "submitter" ]; 
         then yarn build:submitter; 
     else echo invalid env detected, please set BUILD_APP; 
 fi
