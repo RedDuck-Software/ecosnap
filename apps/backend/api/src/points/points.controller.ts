a1b1361f9d6359a610aee178b0bfe385d55015a9
irubyeye <irubyeye@gmail.com>
2024-06-17 22:38:06 +0300

    feat: points module

---
+++ apps/backend/api/src/points/points.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { ApiTags } from '@nestjs/swagger';
+import { PointsService } from './points.service';
+import { UseUserAuthGuard } from '../guards/user-auth.guard';
+import 'multer';
+import { RequestUser, UserClaims } from '../decorators/request-user.decorator';
+
+export const GC_API_TAG = 'Points';
+
+@Controller('points')
+@ApiTags(GC_API_TAG)
+export class PointsController {
+  constructor(private readonly pointsService: PointsService) {}
+
+  @Get('/')
+  @UseUserAuthGuard()
+  async postCastVote(@RequestUser() user: UserClaims) {
+    return await this.pointsService.getUserPoints(user.pubKey);
+  }
+}
