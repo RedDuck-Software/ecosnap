f3c2bfd37409866d7b9cdb526112f9aa0be046a4
kostyamospan <kostya04042001@gmail.com>
2024-06-18 11:10:34 +0300

    chore: submitter achievements

--- apps/backend/merkle-submitter/src/submitter/submitter.service.ts
+++ apps/backend/merkle-submitter/src/submitter/submitter.service.ts
@@ -2,7 +2,16 @@ import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { ComputeBudgetProgram, PublicKey, sendAndConfirmTransaction, Transaction } from '@solana/web3.js';
 import { DataSource, Equal, IsNull, Not, Or } from 'typeorm';
 import { StorageService } from '@gc/storage';
-import { File, GarbageCollect, MerkleProof, MerkleSubmission, MerkleTreeType, User } from '@gc/database-gc';
+import {
+  File,
+  GarbageCollect,
+  MerkleProof,
+  MerkleSubmission,
+  MerkleTreeType,
+  SubmissionType,
+  User,
+  UserAchievement,
+} from '@gc/database-gc';
 import { getMerkleProof, getMerkleRoot, getMerkleTree } from '@metaplex-foundation/js';
 import * as borsh from 'borsh';
 import crypto from 'crypto';
@@ -20,23 +29,25 @@ export class SubmitterService {
 
   async process() {
     const resGc = await this.handleGcSubmission();
+    const resAchievements = await this.handleAchievementsSubmission();
     const merkleRepo = this.dataSource.getRepository(MerkleSubmission);
     const userRepo = this.dataSource.getRepository(User);
     const proofsRepo = this.dataSource.getRepository(MerkleProof);
 
-    const allResults = [resGc];
+    const allResults = [resGc, resAchievements];
+    const allSubmissions = allResults.map((v) => v.submissions).flat();
 
     if (!allResults.map((v) => v.submissions).flat().length || !allResults.map((v) => v.toSave).flat().length) {
       this.logger.debug('Nothing to submit, skipping iteration');
       return;
     }
 
-    const merkleRootsToSubmit = [...resGc.submissions].map((v) => ({
+    const merkleRootsToSubmit = allSubmissions.map((v) => ({
       id: v.id,
       rootHash: v.rootHash,
     }));
 
-    const filesToUpload = [...resGc.submissions].map((v) => ({
+    const filesToUpload = allSubmissions.map((v) => ({
       id: v.id,
       fileContent: v.fileContent,
     }));
@@ -52,6 +63,7 @@ export class SubmitterService {
           res.submissions.map((v) => {
             return merkleRepo.create({
               id: v.id,
+              submissionType: v.submissionType,
               merkleRootHash: v.rootHash,
               submissionTxHash: sumbitTx,
               treeType: v.treeType,
@@ -185,6 +197,7 @@ export class SubmitterService {
         id: crypto.randomUUID(),
         leaves: treeData.fullTree.leaves,
         treeType: MerkleTreeType.FULL,
+        submissionType: SubmissionType.GC,
         fileContent: JSON.stringify(treeData.fullTree),
       },
       {
@@ -192,6 +205,7 @@ export class SubmitterService {
         id: crypto.randomUUID(),
         leaves: treeData.rewardsClaimTree.leaves,
         proofs: treeData.rewardsClaimTree.proofs,
+        submissionType: SubmissionType.GC,
         treeType: MerkleTreeType.ONLY_PROOFS,
         fileContent: JSON.stringify(treeData.rewardsClaimTree),
       },
@@ -200,6 +214,50 @@ export class SubmitterService {
     return {
       submissions: submissions,
       toSave: garbageCollectToBatch,
+    };
+  }
+
+  async handleAchievementsSubmission() {
+    const achRepo = this.dataSource.getRepository(UserAchievement);
+    const merkleRepo = this.dataSource.getRepository(MerkleSubmission);
+
+    const achievementsToBatch = await achRepo.find({
+      where: {
+        merkleSubmitted: false,
+        received: true,
+      },
+      relations: {
+        achievement: true,
+      },
+    });
+
+    if (!achievementsToBatch.length) return { submissions: [], toSave: [] };
+
+    const treeData = await this.generateTreeDataForAchievements(achievementsToBatch);
+
+    const submissions = [
+      {
+        rootHash: treeData.fullTree.rootHash,
+        id: crypto.randomUUID(),
+        leaves: treeData.fullTree.leaves,
+        treeType: MerkleTreeType.FULL,
+        submissionType: SubmissionType.ACHIEVEMENTS,
+        fileContent: JSON.stringify(treeData.fullTree),
+      },
+      {
+        rootHash: treeData.rewardsClaimTree.rootHash,
+        id: crypto.randomUUID(),
+        leaves: treeData.rewardsClaimTree.leaves,
+        proofs: treeData.rewardsClaimTree.proofs,
+        treeType: MerkleTreeType.ONLY_PROOFS,
+        submissionType: SubmissionType.ACHIEVEMENTS,
+        fileContent: JSON.stringify(treeData.rewardsClaimTree),
+      },
+    ];
+
+    return {
+      submissions: submissions,
+      toSave: achievementsToBatch,
     };
   }
 
@@ -240,6 +298,69 @@ export class SubmitterService {
         borsh.serialize('u128', id),
         // TODO: multiply by 10 ** 9
         borsh.serialize('u128', pointsGiven),
+      ]);
+    };
+
+    const encodedClaimLeaves = claimLeafs.map(leafToEncoded);
+    const claimMerkleTree = getMerkleTree(encodedClaimLeaves);
+    const rootClaim = claimMerkleTree.getRoot();
+    const claimRootHex = rootClaim.toString('hex');
+
+    const leavesWithPrevRoot = [{ prevRootHash: prevRootHex, claimRootHash: claimRootHex }, ...leafs];
+    const encodedLeaves = leavesWithPrevRoot.map((l) => JSON.stringify(l));
+    const merkleTree = getMerkleTree(encodedLeaves);
+    const root = merkleTree.getRoot();
+    const rootHex = root.toString('hex');
+
+    return {
+      fullTree: {
+        rootHash: rootHex,
+        prevRootHash: prevRootHex,
+        leaves: leavesWithPrevRoot,
+      },
+      rewardsClaimTree: {
+        rootHash: claimRootHex,
+        leaves: claimLeafs,
+        proofs: Object.fromEntries(
+          claimLeafs.map((v) => [v.user, [{ id: v.id, proofs: getMerkleProof(encodedLeaves, leafToEncoded(v)) }]])
+        ),
+      },
+    };
+  }
+
+  async generateTreeDataForAchievements(achievements: UserAchievement[]) {
+    const prevRootHash = Buffer.from(''); // FIXME
+    const prevRootHex = prevRootHash.toString('hex');
+
+    const leafs = achievements.map((ach) => ({
+      user: ach.user.pubKey,
+      achievement: ach.achievement.id,
+      amount: 1,
+    }));
+
+    const claimLeafs = leafs.map(({ achievement, amount, user }, i) => ({
+      achievementId: achievement,
+      id: i,
+      user: user.toBase58(),
+      amount,
+    }));
+
+    const leafToEncoded = ({
+      user,
+      id,
+      achievementId,
+      amount,
+    }: {
+      achievementId: string;
+      amount: number;
+      id: number;
+      user: string;
+    }) => {
+      return Buffer.concat([
+        new PublicKey(user).toBuffer(),
+        borsh.serialize('u128', id),
+        borsh.serialize('u128', achievementId),
+        borsh.serialize('u128', amount),
       ]);
     };
 
