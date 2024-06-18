8b238eeac2b85c9820e216c595ee095f95ae06ed
MrJeleika <den932005@gmail.com>
2024-06-18 14:50:50 +0300

    feat(web): text

--- apps/frontend/web/src/components/home/nfts/nfts.tsx
+++ apps/frontend/web/src/components/home/nfts/nfts.tsx
@@ -2,7 +2,7 @@ export const NFTs = () => {
   return (
     <div className="flex flex-col gap-4 w-full">
       <h2 className="text-light font-semibold text-[16px]">NFT’s Earned</h2>
-      TODO
+      ТУТ БУДУТ КРАСІВІ ДЕВАЧКИ
     </div>
   );
 };
